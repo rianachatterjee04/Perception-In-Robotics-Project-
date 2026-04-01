6d161056ce9de34fc809339307c05c9dfb2dea60
Gypsybots <yvoragun@asu.edu>
2026-04-01 16:41:55 -0700

    Update __init__.py

--- stage2/__init__.py
+++ stage2/__init__.py
@@ -1,1 +1,7 @@
+"""Stage II indoor navigation package built for the Perception-In-Robotics-Project repo."""
+
+from .schemas import QueryRequest, QueryResponse
+from .service import NavigationService
+
+__all__ = ["QueryRequest", "QueryResponse", "NavigationService"]
 
